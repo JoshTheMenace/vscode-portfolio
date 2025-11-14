a6a95d94eb4d682da853aa282b3be7a3871c4e48
Josh <harrypotterhp333@gmail.com>
2025-11-14 12:00:05 -0700

    Add .gitignore file, update image URLs, and allow additional image source in Next.js config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
       'res.cloudinary.com',
       'avatars.githubusercontent.com',
       'imgur.com',
+      'i.imgur.com',
       'd112y698adiu2z.cloudfront.net',
       'img.youtube.com',
     ],
